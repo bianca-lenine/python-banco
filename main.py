fe7a4e8a1997a7c512118f13e1aca7971d0aac6d
lucas-lima-duarte <lucas_16_lima@hotmail.com>
2025-11-10 20:49:38 -0300

    docs: todo list

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 # Banco em Python
 
 def main():
+
+    """
+    Java Python Menu Loop
+    1. Criar uma conta
+    2. Consultar saldo
+    3. Deposito
+    4. Saque
+    5. Sair
+    """
+
     nome = input('Informe seu nome: ')
-    print('Bem vindo ao Python Banco' + nome)
+    print('Bem vindo ao Python Banco' + nome) # Arrumar espaçamento
+
+    """
+    TODO: 
+    - Enriquecer o Onboarding do Cliente (cadastro) via etapas com validações basicas
+    - Separar o cadastro de uma conta em uma classe separada
+    - Salvar o cadastro do usuário para futuras sessoes (arquivo JSON em uma pasta chamada `data`)
+    - Chamar LLM para dar dicas personalizadas para o cliente com base nos dados dele
+    """
 
 main()
