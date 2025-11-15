5770797cfe2378a325da576a177e4d4df7a275cc
Bianca <biancalenineb.schons@hotmail.com>
2025-11-15 16:10:02 -0300

    feat: cadastro de conta

---
+++ cadastro.py
@@ -0,0 +1,11 @@
+class CadastroConta:
+    
+    def __init__(self):
+        self.contas_cadastradas = []
+    
+    def cadastrar (self, nome, numero_conta)
+        conta = {
+            'nome': nome
+            'numero_conta': numero_conta,
+            'saldo': 0.0
+        }
