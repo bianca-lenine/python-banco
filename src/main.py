50ba93960d412f21005bf56adf02c9b0156c3117
lucas-lima-duarte <lucas_16_lima@hotmail.com>
2025-11-17 07:06:54 -0300

    feat: implemented lifespan FastAPI

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
+import uuid
 import models
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request, Response
 from database import engine, SessionLocal
 
 @asynccontextmanager
@@ -18,6 +19,18 @@ def get_db():
         yield db
     finally:
         db.close()
+
+@app.middleware("http")
+async def request_id_logging(request: Request, call_next):
+    response = await call_next(request)
+    request_id = uuid.uuid4()
+    print(request_id)
+    response.headers['Request-Id'] = str(request_id)
+    return response
+
+@app.get('/health')
+async def health_check():
+    return 'App running...'
 
 # # Endpoint de exemplo# @app.post("/account")
 # def create_account(name: str, email: str, db: Session = Depends(get_db)):
